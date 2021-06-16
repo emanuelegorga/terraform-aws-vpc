e785c231add11dc6595ecf9f824c6b15f77e5381
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-17 00:33:14 +0200

    Create SNS Topic, Subscription and Notification resources.

--- instances/instances.tf
+++ instances/instances.tf
@@ -322,3 +322,24 @@ resource "aws_autoscaling_policy" "backend_production_scaling_policy" {
     target_value = 80.0
   }
 }
+
+resource "aws_sns_topic" "webapp_production_autoscaling_alert_topic" {
+  display_name = "WebApp-AutoScaling-Topic"
+  name         = "WebApp-AutoScaling-Topic"
+}
+
+resource "aws_sns_topic_subscription" "webapp_production_autoscaling_sms_subscription" {
+  endpoint  = "+447704577759"
+  protocol  = "sms"
+  topic_arn = aws_sns_topic.webapp_production_autoscaling_alert_topic.arn
+}
+
+resource "aws_autoscaling_notification" "webapp_autoscaling_notification" {
+  group_names   = [aws_autoscaling_group.ec2_public_autoscaling_group.name]
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR"
+  ]
+  topic_arn     = aws_sns_topic.webapp_production_autoscaling_alert_topic.arn
+}
