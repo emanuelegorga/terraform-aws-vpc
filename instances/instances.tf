71be4b7dc181de1f9e33057f50eb2da23b6e63e4
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-16 23:43:33 +0200

    Define webapp Load Balancer

--- instances/instances.tf
+++ instances/instances.tf
@@ -185,3 +185,29 @@ resource "aws_launch_configuration" "ec2_public_launch_configuration" {
     echo "<html><body><h1>Hello from Production Web App at instance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
   EOF
 }
+
+resource "aws_elb" "webapp_load_balancer" {
+  name            = "Production-WebApp-LoadBalancer"
+  internal        = false
+  security_groups = [aws_security_group.elb_security_group.id]
+  subnets         = [
+    data.terraform_remote_state.network_configuration.public_subnet_1_id,
+    data.terraform_remote_state.network_configuration.public_subnet_2_id,
+    data.terraform_remote_state.network_configuration.public_subnet_3_id
+  ]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "HTTP"
+    lb_port = 80
+    lb_protocol = "HTTP"
+  }
+
+  health_check {
+    healthy_threshold   = 5
+    interval            = 30
+    target              = "HTTP:80/index.html"
+    timeout             = 10
+    unhealthy_threshold = 5
+  }
+}
