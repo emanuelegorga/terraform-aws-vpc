9ab9436573da6d1a7c329a28fff5c2e4fddb3084
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-17 00:41:32 +0200

    Fix intendation issues.

--- instances/instances.tf
+++ instances/instances.tf
@@ -17,77 +17,77 @@ data "terraform_remote_state" "network_configuration" {
 }
 
 resource "aws_security_group" "ec2_public_security_group" {
-  name = "EC2-Public-SG"
+  name        = "EC2-Public-SG"
   description = "Internet reaching access for EC2 instances"
-  vpc_id = data.terraform_remote_state.network_configuration.vpc_id
+  vpc_id      = data.terraform_remote_state.network_configuration.vpc_id
 
   ingress {
-    from_port = 80
-    protocol = "TCP"
-    to_port = 80
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
     cidr_blocks = [0.0.0.0/0]
   }
 
   ingress {
-    from_port = 22
-    protocol = "TCP"
-    to_port = 22
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
     cidr_blocks = [2.44.140.117/32]
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = [0.0.0.0/0]
   }
 }
 
 resource "aws_security_group" "ec2_private_security_group" {
-  name = "EC2-Private-SG"
+  name        = "EC2-Private-SG"
   description = "Only allow public SG resources to access these instances"
-  vpc_id = data.terraform_remote_state.network_configuration.vpc_id
+  vpc_id      = data.terraform_remote_state.network_configuration.vpc_id
 
   ingress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = [aws_security_group.ec2_public_security_group.id]
   }
 
   ingress {
-    from_port = 80
-    protocol = "TCP"
-    to_port = 80
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
     cidr_blocks = [0.0.0.0/0]
     description = "Allow health checking for instances using this SG"
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = [0.0.0.0/0]
   }
 }
 
 resource "aws_security_group" "elb_security_group" {
-  name = "ELB-SG"
+  name        = "ELB-SG"
   description = "ELB Security Group"
-  vpc_id = data.terraform_remote_state.network_configuration.vpc_id
+  vpc_id      = data.terraform_remote_state.network_configuration.vpc_id
 
   ingress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = [0.0.0.0/0]
     description = "Allow web traffic to load balancer"
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = [0.0.0.0/0]
   }
 }
