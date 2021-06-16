5f256da5da91b808fc15ae73a8b5602fa43d2bd0
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-16 22:10:27 +0200

    Add variables outputting for remote state reading. Refactor code style with up to date style standards.

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 terraform {
@@ -7,154 +7,154 @@ terraform {
 }
 
 resource "aws_vpc" "production-vpc" {
-  cidr_block           = "${var.vpc_cidr}}"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
 
-  tags {
+  tags = {
     Name = "Production-VPC"
   }
 }
 
 resource "aws_subnet" "public-subnet-1" {
-  cidr_block        = "${var.public_subnet_1_cidr}"
-  vpc_id            = "${aws_vpc.production-vpc.id}}"
+  cidr_block        = var.public_subnet_1_cidr
+  vpc_id            = aws_vpc.production-vpc.id
   availability_zone = "eu-west-1a"
 
-  tags {
+  tags = {
     Name = "Public-Subnet-1"
   }
 }
 
 resource "aws_subnet" "public-subnet-2" {
-  cidr_block        = "${var.public_subnet_2_cidr}"
-  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = var.public_subnet_2_cidr
+  vpc_id            = aws_vpc.production-vpc.id
   availability_zone = "eu-west-1b"
 
-  tags {
+  tags = {
     Name = "Public-Subnet-2"
   }
 }
 
 resource "aws_subnet" "public-subnet-3" {
-  cidr_block        = "${var.public_subnet_3_cidr}"
-  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = var.public_subnet_3_cidr
+  vpc_id            = aws_vpc.production-vpc.id
   availability_zone = "eu-west-1c"
 
-  tags {
+  tags = {
     Name = "Public-Subnet-3"
   }
 }
 
 resource "aws_subnet" "private-subnet-1" {
-  cidr_block        = "${var.private_subnet_1_cidr}"
-  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = var.private_subnet_1_cidr
+  vpc_id            = aws_vpc.production-vpc.id
   availability_zone = "eu-west-1a"
 
-  tags {
+  tags = {
     Name = "Private-Subnet-1"
   }
 }
 
 resource "aws_subnet" "private-subnet-2" {
-  cidr_block        = "${var.private_subnet_2_cidr}"
-  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = var.private_subnet_2_cidr
+  vpc_id            = aws_vpc.production-vpc.id
   availability_zone = "eu-west-1b"
 
-  tags {
+  tags = {
     Name = "Private-Subnet-2"
   }
 }
 
 resource "aws_subnet" "private-subnet-3" {
-  cidr_block        = "${var.private_subnet_3_cidr}"
-  vpc_id            = "${aws_vpc.production-vpc.id}"
+  cidr_block        = var.private_subnet_3_cidr
+  vpc_id            = aws_vpc.production-vpc.id
   availability_zone = "eu-west-1c"
 
-  tags {
+  tags = {
     Name = "Private-Subnet-3"
   }
 }
 
 resource "aws_route_table" "public-route-table" {
-  vpc_id = "${aws_vpc.production-vpc.id}"
-  tags {
+  vpc_id = aws_vpc.production-vpc.id
+  tags = {
     Name = "Public-Route-Table"
   }
 }
 
 resource "aws_route_table" "private-route-table" {
-  vpc_id = "${aws_vpc.production-vpc.id}"
-  tags {
+  vpc_id = aws_vpc.production-vpc.id
+  tags = {
     Name = "Private-Route-Table"
   }
 }
 
 resource "aws_route_table_association" "public-subnet-1-association" {
-  route_table_id       = "${aws_route_table.public-route-table.id}"
-  subnet_id            = "${aws_subnet.public-subnet-1.id}"
+  route_table_id       = aws_route_table.public-route-table.id
+  subnet_id            = aws_subnet.public-subnet-1.id
 }
 
 resource "aws_route_table_association" "public-subnet-2-association" {
-  route_table_id       = "${aws_route_table.public-route-table.id}"
-  subnet_id            = "${aws_subnet.public-subnet-2.id}"
+  route_table_id       = aws_route_table.public-route-table.id
+  subnet_id            = aws_subnet.public-subnet-2.id
 }
 
 resource "aws_route_table_association" "public-subnet-3-association" {
-  route_table_id       = "${aws_route_table.public-route-table.id}"
-  subnet_id            = "${aws_subnet.public-subnet-3.id}"
+  route_table_id       = aws_route_table.public-route-table.id
+  subnet_id            = aws_subnet.public-subnet-3.id
 }
 
 resource "aws_route_table_association" "private-subnet-1-association" {
-  route_table_id       = "${aws_route_table.private-route-table.id}"
-  subnet_id            = "${aws_subnet.private-subnet-1.id}"
+  route_table_id       = aws_route_table.private-route-table.id
+  subnet_id            = aws_subnet.private-subnet-1.id
 }
 
 resource "aws_route_table_association" "private-subnet-2-association" {
-  route_table_id       = "${aws_route_table.private-route-table.id}"
-  subnet_id            = "${aws_subnet.private-subnet-2.id}"
+  route_table_id       = aws_route_table.private-route-table.id
+  subnet_id            = aws_subnet.private-subnet-2.id
 }
 
 resource "aws_route_table_association" "private-subnet-3-association" {
-  route_table_id       = "${aws_route_table.private-route-table.id}"
-  subnet_id            = "${aws_subnet.private-subnet-3.id}"
+  route_table_id       = aws_route_table.private-route-table.id
+  subnet_id            = aws_subnet.private-subnet-3.id
 }
 
 resource "aws_eip" "elastic-ip-for-nat-gw" {
   vpc = true
   associate_with_private_ip = "10.0.0.5"
 
-  tags {
+  tags = {
     Name = "Production-EIP"
   }
 }
 
 resource "aws_nat_gateway" "nat-gw" {
-  allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
-  subnet_id     = "${aws_subnet.public-subnet-1.id}"
+  allocation_id = aws_eip.elastic-ip-for-nat-gw.id
+  subnet_id     = aws_subnet.public-subnet-1.id
 
-  tags {
+  tags = {
     Name = "Production-NAT-GW"
   }
 
-  depends_on = ["aws_eip.elastic-ip-for-nat-gw"]
+  depends_on = [aws_eip.elastic-ip-for-nat-gw]
 }
 
 resource "aws_route" "nat-gw-route" {
-  route_table_id         = "${aws_route_table.private-route-table.id}"
-  nat_gateway_id         = "${aws_nat_gateway.nat-gw.id}"
+  route_table_id         = aws_route_table.private-route-table.id
+  nat_gateway_id         = aws_nat_gateway.nat-gw.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_internet_gateway" "production-igw" {
-  vpc_id = "${aws_vpc.production-vpc.id}"
+  vpc_id = aws_vpc.production-vpc.id
 
-  tags {
+  tags = {
     Name = "Production IGW"
   }
 }
 
 resource "aws_route" "public-internet-gw-route" {
-  route_table_id         = "${aws_route_table.public-route-table.id}"
-  gateway_id             = "${aws_internet_gateway.production-igw.id}"
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.production-igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
