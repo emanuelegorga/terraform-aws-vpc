9e019cbca98883ab9c2d4b00bee1ea1802311a4f
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-16 23:53:09 +0200

    Define Backend Load Balancer resource

--- instances/instances.tf
+++ instances/instances.tf
@@ -197,10 +197,36 @@ resource "aws_elb" "webapp_load_balancer" {
   ]
 
   listener {
-    instance_port = 80
+    instance_port     = 80
     instance_protocol = "HTTP"
-    lb_port = 80
-    lb_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
+
+  health_check {
+    healthy_threshold   = 5
+    interval            = 30
+    target              = "HTTP:80/index.html"
+    timeout             = 10
+    unhealthy_threshold = 5
+  }
+}
+
+resource "aws_elb" "backend_load_balancer" {
+  name            = "Production-Backend-LoadBalancer"
+  internal        = true
+  security_groups = [aws_security_group.elb_security_group.id]
+  subnets         = [
+    data.terraform_remote_state.network_configuration.private_subnet_1_id,
+    data.terraform_remote_state.network_configuration.private_subnet_2_id,
+    data.terraform_remote_state.network_configuration.private_subnet_3_id
+  ]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
   }
 
   health_check {
