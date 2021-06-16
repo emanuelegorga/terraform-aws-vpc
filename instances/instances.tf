762410ed99ac0ca386597fa94610fc1ff01c89eb
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-16 23:31:47 +0200

    Create private EC2 Launch Configuration

--- instances/instances.tf
+++ instances/instances.tf
@@ -141,13 +141,30 @@ data "aws_ami" "launch_configuration_ami" {
   most_recent = true
 
   filter {
-    name = "owner-alias"
+    name   = "owner-alias"
     values = [amazon]
   }
 
   owners = [amazon]
 }
 
+resource "aws_launch_configuration" "ec2_private_launch_configuration" {
+  image_id                    = data.aws_ami.launch_configuration_ami.id
+  instance_type               = var.ec2_instance_type
+  key_name                    = var.key_pair_name
+  associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
+  security_groups             = [aws_security_group.ec2_private_security_group.id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum update -y
+    yum install httpd24 -y
+    service httpd start
+    chkconfig httpd on
+    export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+    echo "<html><body><h1>Hello from Production Backend at instance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
 
 
-
+EOF
+}
--- instances/variables.tf
+++ instances/variables.tf
@@ -10,3 +10,12 @@ variable "remote_state_bucket" {
 variable "remote_state_key" {
   description = "Key name for layer 1 remote state"
 }
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance type to launch"
+}
+
+variable "key_pair_name" {
+  default = "terraform-vpc-course"
+  description = "Keypair to use to connect to EC2 Instances"
+}
