c78a58d3dbe1e54a8e149ea75fa86433eab8b1f7
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-16 21:29:24 +0200

    Create Elastic IP address to be used for Nat Gateway.

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -118,3 +118,12 @@ resource "aws_route_table_association" "private-subnet-3-association" {
   route_table_id = "${aws_route_table.private-route-table.id}"
   subnet_id            = "${aws_subnet.private-subnet-3.id}"
 }
+
+resource "aws_eip" "elastic-ip-for-nat-gw" {
+  vpc = true
+  associate_with_private_ip = "10.0.0.5"
+
+  tags {
+    Name = "Production-EIP"
+  }
+}
