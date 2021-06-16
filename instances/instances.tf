2e2cd0f0fa425ffed1daec8906231d16a3bf6de8
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-16 23:35:15 +0200

    Create public EC2 Launch configuration

--- instances/instances.tf
+++ instances/instances.tf
@@ -164,7 +164,24 @@ resource "aws_launch_configuration" "ec2_private_launch_configuration" {
     chkconfig httpd on
     export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
     echo "<html><body><h1>Hello from Production Backend at instance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
+  EOF
+}
 
+resource "aws_launch_configuration" "ec2_public_launch_configuration" {
+  image_id                    = data.aws_ami.launch_configuration_ami.id
+  instance_type               = var.ec2_instance_type
+  key_name                    = var.key_pair_name
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
+  security_groups             = [aws_security_group.ec2_public_security_group.id]
 
-EOF
+  user_data = <<EOF
+    #!/bin/bash
+    yum update -y
+    yum install httpd24 -y
+    service httpd start
+    chkconfig httpd on
+    export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+    echo "<html><body><h1>Hello from Production Web App at instance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
+  EOF
 }
