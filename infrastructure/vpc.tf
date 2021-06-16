835bc7992426b6608f52edf985804cc12fda06b6
Emanuele Gorga <emanuelegorga@gmail.com>
2021-06-16 21:38:18 +0200

    Create internet Gateway and add it to route table. Adjust wrong indentation to existing resources.

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -90,32 +90,32 @@ resource "aws_route_table" "private-route-table" {
 }
 
 resource "aws_route_table_association" "public-subnet-1-association" {
-  route_table_id = "${aws_route_table.public-route-table.id}"
+  route_table_id       = "${aws_route_table.public-route-table.id}"
   subnet_id            = "${aws_subnet.public-subnet-1.id}"
 }
 
 resource "aws_route_table_association" "public-subnet-2-association" {
-  route_table_id = "${aws_route_table.public-route-table.id}"
+  route_table_id       = "${aws_route_table.public-route-table.id}"
   subnet_id            = "${aws_subnet.public-subnet-2.id}"
 }
 
 resource "aws_route_table_association" "public-subnet-3-association" {
-  route_table_id = "${aws_route_table.public-route-table.id}"
+  route_table_id       = "${aws_route_table.public-route-table.id}"
   subnet_id            = "${aws_subnet.public-subnet-3.id}"
 }
 
 resource "aws_route_table_association" "private-subnet-1-association" {
-  route_table_id = "${aws_route_table.private-route-table.id}"
+  route_table_id       = "${aws_route_table.private-route-table.id}"
   subnet_id            = "${aws_subnet.private-subnet-1.id}"
 }
 
 resource "aws_route_table_association" "private-subnet-2-association" {
-  route_table_id = "${aws_route_table.private-route-table.id}"
+  route_table_id       = "${aws_route_table.private-route-table.id}"
   subnet_id            = "${aws_subnet.private-subnet-2.id}"
 }
 
 resource "aws_route_table_association" "private-subnet-3-association" {
-  route_table_id = "${aws_route_table.private-route-table.id}"
+  route_table_id       = "${aws_route_table.private-route-table.id}"
   subnet_id            = "${aws_subnet.private-subnet-3.id}"
 }
 
@@ -140,7 +140,21 @@ resource "aws_nat_gateway" "nat-gw" {
 }
 
 resource "aws_route" "nat-gw-route" {
-  route_table_id = "${aws_route_table.private-route-table.id}"
-  nat_gateway_id = "${aws_nat_gateway.nat-gw.id}"
+  route_table_id         = "${aws_route_table.private-route-table.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat-gw.id}"
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = "${aws_vpc.production-vpc.id}"
+
+  tags {
+    Name = "Production IGW"
+  }
+}
+
+resource "aws_route" "public-internet-gw-route" {
+  route_table_id         = "${aws_route_table.public-route-table.id}"
+  gateway_id             = "${aws_internet_gateway.production-igw.id}"
   destination_cidr_block = "0.0.0.0/0"
 }
